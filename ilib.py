f9597da115597fe14b3bc15421b75105a46f14bf
David Castillo <daveecastillo@gamil.com>
2013-06-08 14:35:27 -0700

    Refactored and added check for excluding multi message(threads) emails

--- ilib.py
+++ ilib.py
@@ -1,11 +1,42 @@
+"""This file was used for testing purposes.
+"""
 import imaplib
 import os
+import email
 
 
 def open_connection(verbose=False):
     connection = imaplib.IMAP4_SSL('imap.gmail.com', '993')
-    username = os.environ['EMAIL_ACCOUNT']
-    password = os.environ['EMAIL_PASSWORD']
+    username = os.environ['E_READ_ACCOUNT']
+    password = os.environ['E_READ_PASSWORD']
 
     connection.login(username, password)
     return connection
+
+connection = imaplib.IMAP4_SSL('imap.gmail.com', '993')
+username = os.environ['E_READ_ACCOUNT']
+password = os.environ['E_READ_PASSWORD']
+
+connection.login(username, password)
+connection.select("inbox")
+# result, unseen_email = connection.search(None, 'UNSEEN')
+
+result, unseen_email = connection.uid('search', None, 'UNSEEN')
+
+uids = unseen_email[0]
+uid_list = uids.split(' ')
+
+for uid in uid_list:
+    result, data = connection.uid('fetch', uid, '(X-GM-THRID X-GM-MSGID)')
+    # if it was an unseen  and not threaded email, save the email
+    if result == 'OK' and not data == '':
+        raw_email = data[0][1]
+        email_message = email.message_from_string(raw_email)
+        # The 1 index of the parseaddre gives us the email address
+        senders_email = email.utils.parseaddr(email_message['From'])[1]
+
+        subject = email_message.get('Subject')
+
+
+if __name__ == '__main__':
+    pass
--- remail/management/commands/get_email.py
+++ remail/management/commands/get_email.py
@@ -9,37 +9,50 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # imap protocol docs:
         # http://tools.ietf.org/html/rfc3501
+        connection = create_connection()
 
-        obj = imaplib.IMAP4_SSL('imap.gmail.com', '993')
-        username = os.environ['EMAIL_ACCOUNT']
-        password = os.environ['EMAIL_PASSWORD']
+        connection.select("inbox")
 
-        obj.login(username, password)
-        obj.select("inbox")
-        result, unseen_email = obj.search(None, "UNSEEN")
+        result, unseen_email = connection.search(None, "UNSEEN")
 
-        # if there are unseen emails get unseen emails and parse
-        # and save info
+        # if there are unseen emails parse and save info
         if result == 'OK' and not unseen_email[0] == '':
-            ids = unseen_email[0]
-            id_list = ids.split(' ')
-            parse_ids_and_save_emails(id_list, obj)
+            parse_ids_and_save_emails(unseen_email, connection)
+
+        close_connection(connection)
 
 
-def parse_ids_and_save_emails(id_list, obj):
+def create_connection():
+    connection = imaplib.IMAP4_SSL('imap.gmail.com', '993')
+    username = os.environ['EMAIL_ACCOUNT']
+    password = os.environ['EMAIL_PASSWORD']
+    connection.login(username, password)
+    return connection
+
+
+def close_connection(connection):
+    connection.close()
+    connection.logout()
+
+
+def parse_ids_and_save_emails(unseen_email, obj):
+    ids = unseen_email[0]
+    id_list = ids.split(' ')
+
     for email_id in id_list:
         result, data = obj.fetch(email_id, "(RFC822)")
         raw_email = data[0][1]
         email_message = email.message_from_string(raw_email)
+        # Save the message if the message does not have a thread
+        if not email_message.is_multipart():
+            # The 1 index of the parseaddr gives us the address
+            senders_email = email.utils.parseaddr(email_message['From'])[1]
 
-        # The 1 index of the parseaddr gives us the address
-        senders_email = email.utils.parseaddr(email_message['From'])[1]
+            # Subject does not get parsed like email since
+            # I'm only interested in the whole subject string.
+            subject = email_message.get('Subject')
 
-        # Subject does not get parsed like email since
-        # I'm only interested in the whole subject string.
-        subject = email_message.get('Subject')
-
-        e = Email(subject=subject,
-                  senders_email=senders_email,
-                  body=raw_email)
-        e.save()
+            e = Email(subject=subject,
+                      senders_email=senders_email,
+                      body=raw_email)
+            e.save()
